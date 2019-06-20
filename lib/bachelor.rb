aa767615784019f936fc6cdbfb5af6a7995adcd9
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-20 23:16:26 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,7 @@ def get_first_name_of_season_winner(data, season)
     if seasons == season
       contestants.each do |info|
         if info.has_key?("name")
-          contestantName = data[seasons][info]
+          contestantName = info.values
         end
         if info.has_value?("Winner")
           winner = contestantName
