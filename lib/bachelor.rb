49b29cba74c108037b35c88de8fbf3eb37b0844d
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-20 23:39:11 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_occupation(data, hometown)
   occupation = ""
   data.each do |season, contestants|
     contestants.each do |info|
-      if info.values[2] == hometown
+      if info.values[2] == hometown && occupation == ""
         occupation = info.values[3]
       end
     end
@@ -59,4 +59,5 @@ end
 
 def get_average_age_for_season(data, season)
   # code here
+  
 end
