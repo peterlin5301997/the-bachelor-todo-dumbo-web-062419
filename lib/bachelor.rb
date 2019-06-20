204e815934f8d299a77d5dccd73c9fc84c27a0ef
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-20 23:42:17 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -60,10 +60,12 @@ end
 def get_average_age_for_season(data, season)
   # code here
   sumAge = 0
-  data.each do |season, contestants|
-    contestants.each do |info|
-      if info.keys[1] == "age"
-        sumAge += info.values[1].to_i
+  data.each do |seasons, contestants|
+    if seasons == season
+      contestants.each do |info|
+        if info.keys[1] == "age"
+          sumAge += info.values[1].to_i
+        end
       end
     end
   end
